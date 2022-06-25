e614149a4125772d8db8d8784c04dd635838f669
Denis Levy <denis.levi11@gmail.com>
2022-06-24 21:29:05 -0300

    010 - Fix icon background color

--- src/components/ExtensionLinkComponent/index.tsx
+++ src/components/ExtensionLinkComponent/index.tsx
@@ -10,9 +10,9 @@ export const ExtensionLinkComponent: React.FC<ExtensionLinkComponentProps> = ({
   return (
     <a
       href={href}
-      className="flex items-center bg-grey-700 hover:bg-grey-600 rounded overflow-hidden cursor-pointer w-full justify-between"
+      className="flex bg-grey-700 hover:bg-grey-600 rounded overflow-hidden cursor-pointer w-full justify-between text-center"
     >
-      <div className="flex px-6 h-full bg-green-500 items-center justify-center">
+      <div className="flex px-6 bg-green-500 items-center justify-center">
         {icon}
       </div>
       <div className="flex flex-col gap-2 px-6 py-3">
--- src/components/SidebarComponent/index.tsx
+++ src/components/SidebarComponent/index.tsx
@@ -6,7 +6,7 @@ export const SidebarComponent: React.FC = () => {
   const { data } = useGetLessonsQuery();
   const { slug } = useParams();
   return (
-    <aside className="hidden md:block min-w-[348px] max-h-[1190px] bg-grey-700 p-6 border-l border-grey-600 overflow-y-scroll">
+    <aside className="hidden md:block min-w-[348px] max-h-[200vh] bg-grey-700 p-6 border-l border-grey-600 overflow-y-scroll">
       <h3 className="font-bold text-2xl pb-6 border-b border-grey-600 mb-6 block">
         Cronograma de aulas
       </h3>
